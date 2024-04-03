66883bd808a0dbebb856ce4d228bf6648c9c66be
kevy60 <kevin.vorsmann@voco.ee>
2024-04-03 10:38:51 +0300

    8.  Hard-coded discountDelivers #https://www.pivotaltracker.com/story/show/187267171] - Kunter

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Näitab toodet ja hinda
+let quantity = "68";
+let price = "9.50";
+console.log("Initial price: ", price, "Quantity: ", quantity);
 
-quantity = "68" 
-price = "9.50"
-console.log(price, quantity);
+let orderValue; // Declare orderValue globally
 
 // See siis arvutab tellimuse hinna
 function calculateOrderValue(quantity, price, stateCode) {
@@ -17,17 +18,25 @@ function calculateOrderValue(quantity, price, stateCode) {
     // Kontrollib siis kas osariigi kood on defineeritud
     if (salesTaxRates.hasOwnProperty(stateCode)) {
         // Arvutab tellimuse hinna ilma müügimaksuta
-        let orderValue = quantity * price;
+        orderValue = quantity * price;
         let salesTax = orderValue * salesTaxRates[stateCode];
         orderValue += salesTax;
-        return orderValue;
+
+        // Kindel allahindlus
+        const discountPercentance = 20;
+        const discountPrice = orderValue - (orderValue * (discountPercentance / 100));
+
+        return { orderValue, discountPrice };
     } else {
         return "Osariigi kood ei ole määratud.";
     }
 }
 
+let result = calculateOrderValue(5, 10, "AR");
 
-
-
-console.log(calculateOrderValue(5, 10, "AR"));
-
+if (typeof result === "object") {
+    console.log("Order Value: ", result.orderValue);
+    console.log("Discount Price: ", result.discountPrice);
+} else {
+    console.log(result);
+}
